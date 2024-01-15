ba6b4c30c911237cd7676bdf23b00fb047e0c489
Roma <romankechko98@yandex.ru>
2024-01-15 18:11:14 +0600

    fix: webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,62 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-let mode = 'development';
-if (process.env.NODE_ENV === 'production') {
-  mode = 'production';
+let mode = "development";
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
 }
 
 module.exports = {
   mode: mode,
   output: {
-    assetModuleFilename: "assets/[hash][ext][query]",
+    assetModuleFilename: "assets/[name][ext][query]",
     clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html"
-    })],
+      template: "./index.html",
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.(css)$/,
-        use: [
-          "style-loader",
-          "css-loader",
-        ]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(docx|php)$/i,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: '[path][name].[ext]',
+          name: "[path][name].[ext]",
         },
       },
       {
         test: /\.htaccess$/i,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: '.htaccess',
+          name: ".htaccess",
         },
       },
       {
         test: /\.html$/,
-        loader: 'html-loader',
+        loader: "html-loader",
         options: {
           sources: {
             list: [
-              '...',
+              "...",
               {
-                tag: 'a',
-                attribute: 'href',
-                type: 'src',
+                tag: "a",
+                attribute: "href",
+                type: "src",
                 filter: (tag, attribute, attributes, resourcePath) => {
-                  return attributes.map((x) => 'data-fancybox' == x['name']).some(item => item);
-                }
-              }
-            ]
-          }
-        }
-      }
-    ]
-  }
-}
+                  return attributes
+                    .map((x) => "data-fancybox" == x["name"])
+                    .some((item) => item);
+                },
+              },
+            ],
+          },
+        },
+      },
+    ],
+  },
+};
