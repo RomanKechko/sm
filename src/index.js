a2676f0e326e069ebdcb6da7a29f0ec3415e989b
Roma <romankechko98@yandex.ru>
2024-01-15 18:01:18 +0600

    add gh-pages

--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-import '../pages/index.css';
-require('../scripts/script');
-require('../scripts/mail_script');
-require('../files/Smartcompletion.docx');
-require('../.htaccess');
-require('../send_mail.php');
